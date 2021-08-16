38bff1f79e87ab7f2f5ac33ecb5107e599568cac
Sergio Junior <sergio_jfj@hotmail.com>
2021-08-16 20:09:16 -0300

    ajuste app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import Topo from "./components/Topo";
 import Home from "./components/Home";
 import Frontend from "./components/Frontend";
@@ -12,88 +11,70 @@ import NotFound from "./components/NotFound";
 import Livro from "./components/Livro";
 import axios from "axios";
 
-
 class App extends Component {
   state = {
     livros: [],
   };
-  /* //Biblioteca API Fetch Array
-  componentDidMount(){
-    fetch("/api/todosOsLivros.json")
-    .then(response => response.json())
-    .then(livros => this.setState({livros}))
-    .catch(function(error){
+  async componentDidMount() {
+    try {
+      const { data: livros } = await axios.get("/api/todosOsLivros.json");
+      this.setState({ livros });
+    } catch (error) {
+      console.log(error);
       document
-      .querySelectorAll("main")[0]
-      .insertAdjacentHTML("beforeend", "<p class='alerta'>Mensagem de Erro</p>");
-    })
-    .finally(function(){
-      console.log("Sempre retorna")
-    })
-  }
-  */
-
-  async componentDidMount(){ // Biblioteca API Axios, melhor recomendada
-      try {
-        const { data: livros } = await axios.get("/api/todosOsLivros.json");
-        this.setState({ livros });
-      } catch (error) {
-        console.log(error);
-        document
-          .querySelectorAll(".principal")[0]
-          .insertAdjacentHTML(
-            "beforeend",
-            "<p class='erro'>Mensagem de erro</p>"
-          );
-      }
-    }
-
-    render() {
-      return (
-        <Router>
-          <Topo />
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={() => <Home livros={this.state.livros} />}
-            />
-            <Route
-              exact
-              path="/frontend"
-              render={() => <Frontend livros={this.state.livros} />}
-            />
-            <Route
-              exact
-              path="/programacao"
-              render={() => <Programacao livros={this.state.livros} />}
-            />
-            <Route
-              exact
-              path="/design"
-              render={() => <Design livros={this.state.livros} />}
-            />
-            <Route
-              exact
-              path="/catalogo"
-              render={() => <Catalogo livros={this.state.livros} />}
-            />
-            <Route
-              path="/livro/:livroSlug"
-              render={(props) => {
-                const livro = this.state.livros.find(
-                  (livro) => livro.slug === props.match.params.livroSlug
-                );
-                if (livro) return <Livro livro={livro} />;
-                else return <NotFound />;
-              }}
-            />
-            <Route component={NotFound} />
-          </Switch>
-          <Rodape />
-        </Router>
-      );
+        .querySelectorAll(".principal")[0]
+        .insertAdjacentHTML(
+          "beforeend",
+          "<p class='erro'>Mensagem de erro</p>"
+        );
     }
   }
-  
-  export default App;
+  render() {
+    return (
+      <Router>
+        <Topo />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <Home livros={this.state.livros} />}
+          />
+          <Route
+            exact
+            path="/frontend"
+            render={() => <Frontend livros={this.state.livros} />}
+          />
+          <Route
+            exact
+            path="/programacao"
+            render={() => <Programacao livros={this.state.livros} />}
+          />
+          <Route
+            exact
+            path="/design"
+            render={() => <Design livros={this.state.livros} />}
+          />
+          <Route
+            exact
+            path="/catalogo"
+            render={() => <Catalogo livros={this.state.livros} />}
+          />
+          <Route
+            path="/livro/:livroSlug"
+            render={(props) => {
+              const livro = this.state.livros.find(
+                (livro) => livro.slug === props.match.params.livroSlug
+              );
+              if (livro) return <Livro livro={livro} />;
+              else return <NotFound />;
+            }}
+          />
+          <Route component={NotFound} />
+        </Switch>
+        <Rodape />
+      </Router>
+    );
+  }
+}
+
+export default App;
