e72fe6f8e4626ce91d4bc7ec78bbd7ece5e1a688
Sergio Junior <sergio_jfj@hotmail.com>
2021-08-16 17:02:45 -0300

    primeiro commit

---
+++ src/components/Principal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./Home";
+
+const Principal = () =>{
+    return(
+        <main className="principal">
+            <Home/> 
+        </main>
+        );
+};
+//Aqui serão os componentes da Home Page do site
+export default Principal;
